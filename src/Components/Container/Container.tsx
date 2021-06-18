b4bad4c98f5d257b20f36840a391b7205786784f
karachunvladislav <karachunvladislav@gmail.com>
2021-06-18 17:58:13 +0300

    feat: added library, added back btn in container

--- src/Components/Container/Container.tsx
+++ src/Components/Container/Container.tsx
@@ -1,7 +1,9 @@
 import styles from "./index.module.scss";
 
-import React, { FC, ReactElement } from "react";
-import { useLocation } from "react-router-dom";
+import React, { FC, ReactElement, useEffect, useState } from "react";
+import { useLocation, useHistory } from "react-router-dom";
+
+import { MetCircleBtn } from "@metamodern.dev/metamodern-ui/";
 
 import { ContainerProps } from "./index";
 
@@ -15,13 +17,30 @@ const headerList = {
 
 export const Container: FC<ContainerProps> = ({ children }): ReactElement => {
     const location = useLocation();
+    const history = useHistory();
+
+    const [isHomePage, setIsHomePage] = useState(true);
 
     const header = headerList[location.pathname];
+
+    useEffect(() => {
+        setIsHomePage(location.pathname === routConstants.HOME);
+    }, [location.pathname, setIsHomePage]);
+
+    const setPath = () => {
+        history.push(routConstants.HOME);
+    };
 
     return (
         <div className={styles.ground}>
             <div className={styles.ground__centred}>
                 <div className={styles.ground__centred__header}>
+                    {isHomePage ? null : (
+                        <div className={styles.btnContainer}>
+                            <MetCircleBtn onClick={setPath} />
+                        </div>
+                    )}
+
                     <span className="adminkaH5">{header}</span>
                 </div>
                 <div className={styles.ground__centred__container}>
--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -2,3 +2,10 @@ declare module "*.scss" {
     const content: { [className: string]: string };
     export = content;
 }
+
+// declare module "react" {
+//     interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
+//         // extends React's HTMLAttributes
+//         custom?: string;
+//     }
+// }
