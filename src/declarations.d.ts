8b41b1df7f4accc798d802483a30a5a1847f2dc5
karachunvladislav <karachunvladislav@gmail.com>
2021-06-21 12:27:16 +0300

    gh-pages

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -2,10 +2,3 @@ declare module "*.scss" {
     const content: { [className: string]: string };
     export = content;
 }
-
-// declare module "react" {
-//     interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
-//         // extends React's HTMLAttributes
-//         custom?: string;
-//     }
-// }
